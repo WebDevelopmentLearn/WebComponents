3ee2ffdc613585f71282d43efe3f3e21c354e827
007killer2 <savenko.artem@gmx.de>
2024-06-11 19:46:09 +0200

    Close modal, if pressed key is Escape and reset input, if pressed key is Escape and close button

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,41 @@
 const openModalBtn = document.querySelector(".openModalBtn");
+const closeModalBtn = document.querySelector(".closeModalBtn");
 const modal = document.querySelector("#modal");
+const modalBody = document.querySelector("#modalBody");
 
+const input = document.querySelector("input");
 openModalBtn.addEventListener("click", (event) => {
     event.preventDefault();
     modal.classList.toggle("modalHidden");
-})
+});
+
+closeModalBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    modal.classList.toggle("modalHidden");
+    input.value = "";
+});
+
 
 modal.addEventListener("click", (event) => {
     event.preventDefault();
-    if (event.target === modal) {
+    modal.classList.toggle("modalHidden");
+});
+
+modalBody.addEventListener("click", (event) => {
+    event.preventDefault();
+    event.stopPropagation();
+});
+
+
+document.addEventListener("keydown", (event) => {
+    if (event.key === "Escape") {
+        modal.classList.toggle("modalHidden");
+        input.value = "";
+    }
+    else if (event.key === "Enter") {
         modal.classList.toggle("modalHidden");
     }
-})
-
-
+});
 
 
 
