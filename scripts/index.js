e1db642b7bb9ad31dbbad41a4603995f14fa1fb2
007killer2 <savenko.artem@gmx.de>
2024-06-12 22:17:46 +0200

    Тестовая реализация диалогового окна при клике по оверлею модального окна

--- scripts/index.js
+++ scripts/index.js
@@ -17,6 +17,10 @@ closeModalBtn.addEventListener("click", (event) => {
 
 modal.addEventListener("click", (event) => {
     event.preventDefault();
+    if (input.value) {
+        console.log(2121);
+        openAdditionModal();
+    }
     modal.classList.toggle("modalHidden");
 });
 
@@ -37,6 +41,36 @@ document.addEventListener("keydown", (event) => {
     }
 });
 
+
+function openAdditionModal() {
+    const additionModal = document.createElement("div");
+    additionModal.classList.add("modal");
+    
+    const additionModalBody = document.createElement("div");
+    additionModalBody.id = "modalBody2";
+    const additionModalPar = document.createElement("p");
+    const additionModalYesBtn = document.createElement("button");
+    const additionModalCancelBtn = document.createElement("button");
+
+    additionModalYesBtn.addEventListener("click", (event) => {
+        event.preventDefault();
+        input.value = "";
+        additionModal.classList.toggle("modalHidden");
+    })
+
+    additionModalCancelBtn.addEventListener("click", (event) => {
+        event.preventDefault();
+
+        additionModal.classList.toggle("modalHidden");
+        modal.classList.toggle("modalHidden");
+    })
+    additionModalPar.textContent = "Вы уверены, что хотите закрыть это окно? Введенные данные будут утеряны";
+    additionModalYesBtn.textContent = "Закрыть";
+    additionModalCancelBtn.textContent = "Отмена";
+    additionModalBody.append(additionModalPar, additionModalYesBtn, additionModalCancelBtn);
+    additionModal.append(additionModalBody);
+    document.body.append(additionModal);
+}
 
 
 
