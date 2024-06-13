65150f47bf0dbe4308d56c33d73c5be9830ff45a
007killer2 <savenko.artem@gmx.de>
2024-06-13 16:35:37 +0200

    Доп модальные окна

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,11 @@ const openModalBtn = document.querySelector(".openModalBtn");
 const closeModalBtn = document.querySelector(".closeModalBtn");
 const modal = document.querySelector("#modal");
 const modalBody = document.querySelector("#modalBody");
-const input = document.querySelector("input");
+const loginInput = document.querySelector("#loginInput");
+const passwordInput = document.querySelector("#passwordInput");
+
+const loginForm = document.querySelector(".loginForm");
+const loginBtn = document.querySelector("#loginBtn");
 openModalBtn.addEventListener("click", (event) => {
     event.preventDefault();
     modal.classList.toggle("modalHidden");
@@ -11,14 +15,14 @@ openModalBtn.addEventListener("click", (event) => {
 closeModalBtn.addEventListener("click", (event) => {
     event.preventDefault();
     modal.classList.toggle("modalHidden");
-    input.value = "";
+    loginInput.value = "";
+    passwordInput.value = "";
 });
 
 
 modal.addEventListener("click", (event) => {
     event.preventDefault();
-    if (input.value) {
-        console.log(2121);
+    if (loginInput.value || passwordInput.value) {
         openAdditionModal();
     }
     modal.classList.toggle("modalHidden");
@@ -33,13 +37,59 @@ modalBody.addEventListener("click", (event) => {
 document.addEventListener("keydown", (event) => {
     if (event.key === "Escape" && modal.checkVisibility()) {
         modal.classList.toggle("modalHidden");
-        input.value = "";
+        loginInput.value = "";
+        passwordInput.value = "";
     }
 
     if (event.key === "Enter") {
         modal.classList.toggle("modalHidden");
     }
 });
+
+
+loginBtn.addEventListener("click", () => {
+    const loginSuccessModal = document.createElement("div");
+    const lineDiv = document.createElement("div");
+    lineDiv.classList.add("lineDiv");
+    loginSuccessModal.classList.add("modal");
+
+    const loginSuccessModalBody = document.createElement("div");
+    loginSuccessModalBody.id = "modalBody2";
+    const loginSuccessModalPar = document.createElement("p");
+    const loginSuccessModalYesBtn = document.createElement("button");
+    // const additionModalCancelBtn = document.createElement("button");
+
+    loginSuccessModalYesBtn.addEventListener("click", (event) => {
+        event.preventDefault();
+        // loginInput.value = "";
+        // passwordInput.value = "";
+        loginSuccessModal.classList.toggle("modalHidden");
+    });
+
+
+    //background-color: #15ca02;
+    //box-shadow: 0px 1px 3px 0px #16df02;
+
+    //background-color: rgb(202 2 2 / 82%);
+    //box-shadow: 0px 1px 3px 0px #df0202bd;
+
+
+
+    //
+    // additionModalCancelBtn.addEventListener("click", (event) => {
+    //     event.preventDefault();
+    //
+    //     additionModal.classList.toggle("modalHidden");
+    //     modal.classList.toggle("modalHidden");
+    // })
+    loginSuccessModalPar.textContent = "Test";
+    loginSuccessModalYesBtn.textContent = "Закрыть";
+    // loginSuccessModalCancelBtn.textContent = "Отмена";
+    loginSuccessModalBody.append(lineDiv, loginSuccessModalPar, loginSuccessModalYesBtn);
+    loginSuccessModal.append(loginSuccessModalBody);
+    document.body.append(loginSuccessModal);
+})
+
 
 
 function openAdditionModal() {
@@ -54,7 +104,8 @@ function openAdditionModal() {
 
     additionModalYesBtn.addEventListener("click", (event) => {
         event.preventDefault();
-        input.value = "";
+        loginInput.value = "";
+        passwordInput.value = "";
         additionModal.classList.toggle("modalHidden");
     })
 
@@ -71,11 +122,3 @@ function openAdditionModal() {
     additionModal.append(additionModalBody);
     document.body.append(additionModal);
 }
-
-
-
-
-
-
-
-
