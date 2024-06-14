cd726bc9e24a467dea34e853e75ae5564e3822c2
007killer2 <savenko.artem@gmx.de>
2024-06-14 12:18:24 +0200

    Simple animation for dialog window

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,37 @@
 const dialogWindow = document.querySelector(".dialogWindow");
 const openDialogWindowBtn = document.querySelector("button");
-
+const sendInfoBtn = document.querySelector(".sendInfoBtn");
+const usernameInput = document.querySelector("#username");
+const cuponInput = document.querySelector("#cupon");
 function showDialog(event) {
-    dialogWindow.classList.remove("dialogWindowHidden");
-}
-
-function closeDialog(event) {
-    if (event.key === "Escape") {
-        dialogWindow.classList.add("dialogWindowHidden");
+    if (dialogWindow) {
+        const dialog = document.querySelector(".dialogWindow.open");
+        if (dialog) {
+            closeDialog(event);
+        }
+        dialogWindow.classList.add("open");
     }
 }
 
-openDialogWindowBtn.addEventListener("click", showDialog);
+function closeDialogEscape(event) {
+    if (dialogWindow) {
+        if (event.key === "Escape") {
+            dialogWindow.classList.remove("open");
+        }
+    }
+}
 
-window.addEventListener("keydown", closeDialog);
+function closeDialog(event) {
+    event.preventDefault();
+    if (dialogWindow) {
+        dialogWindow.classList.remove("open");
+        usernameInput.value = "";
+        cuponInput.value = "";
+    }
+}
+
+
+openDialogWindowBtn.addEventListener("click", showDialog);
+sendInfoBtn.addEventListener("click", closeDialog);
+
+window.addEventListener("keydown", closeDialogEscape);
