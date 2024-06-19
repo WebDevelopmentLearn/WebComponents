9c7b86a9b4218c7f9ce3525bb8997c9e7def1cac
007killer2 <savenko.artem@gmx.de>
2024-06-19 19:23:33 +0200

    Реализация модального окна

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,48 @@
 const openModalCreateOrderBtn = document.querySelector(".openCreateOrderModalBtn");
-const modalOverlay = document.querySelector(".modalOverlay");
+const modalCreateOrderOverlay = document.querySelector(".modalCreateOrderOverlay");
+const modalCreateOrderWindow = document.querySelector(".modalCreateOrderWindow");
+const createOrderBtn = document.querySelector(".createOrderBtn");
 
-openModalCreateOrderBtn.addEventListener("click", () => {
-  modalOverlay.classList.add("open");
+const createOrderAddressInput = document.querySelector("createOrderAddressInput");
+const createOrderPriceInput = document.querySelector("createOrderPriceInput");
+const createOrderRecipientInput = document.querySelector("createOrderRecipientInput");
+
+const hasDigits = /\d/; //регулярное выражение, которое проверяет наличие цифр в строке.
+const hasLetters = /[a-zA-Z]/; //регулярное выражение, которое проверяет наличие букв в строке.
+
+openModalCreateOrderBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  modalCreateOrderOverlay.classList.add("open");
 });
+
+modalCreateOrderOverlay.addEventListener("click", (event) => {
+  event.preventDefault();
+  modalCreateOrderOverlay.classList.remove("open");
+});
+
+modalCreateOrderWindow.addEventListener("click", (event) => {
+  event.stopPropagation();
+});
+
+createOrderBtn.addEventListener("submit", (event) => {
+  event.preventDefault();
+  if (validateRecipient(createOrderRecipientInput.value) && validatePrice(createOrderPriceInput.value)) {
+    console.log("Test");
+  }
+});
+
+function validateRecipient(username) {
+  let flag = false;
+  for (let i = 0; i < username.length; i++) {
+    const element = username[i];
+    if (hasDigits.test(element) || !hasLetters.test(element)) {
+      flag = true;
+      break;
+    }
+  }
+  return !flag && username.length >= 2 && username.length <= 24;
+}
+
+function validatePrice(price) {
+  return hasDigits.test(price);
+}
